f9d5b38523fc8e97773d3f8bb869acf3154ae530
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-01-16 12:39:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,6 +60,12 @@ class BinarySearchTree:
         result = []
         self._inorder_traversal(self.root, result)
         return result
+        
+    def _inorder_traversal(self, node, result):
+        if node:
+            self._inorder_traversal(node.left, result)
+            result.append(node.key)
+            self._inorder_traversal(node.right, result)
 
 
 
