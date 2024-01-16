8514bfead965a4102448e93524fb9a42fa20802f
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-01-16 12:32:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ class TreeNode:
         self.key = key
         self.left = None
         self.right = None
+
+    def __str__(self):
+        return str(self.key)
         
 class BinarySearchTree:
     def __init__(self):
